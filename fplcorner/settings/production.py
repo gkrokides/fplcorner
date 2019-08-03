1b26d636a650ac0b700a0499f18b8e2e80fe2c9b
gkrokides <georgekrokides@hotmail.com>
2019-08-03 10:18:33 +0300

    a few modifications in settings

--- fplcorner/settings/production.py
+++ fplcorner/settings/production.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4w!ud+9d+)jv@$^!ge7@&x0u-)q-ph@f1v4=v-gikl7v^zr+xi'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
