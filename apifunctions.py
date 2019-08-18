82d845426528d17e78aa8d84b9d835f649498aa6
gkrokides <georgekrokides@hotmail.com>
2019-08-18 15:52:17 +0300

    modified gameweek stats

--- apifunctions.py
+++ apifunctions.py
@@ -28,8 +28,8 @@ def fpl_fixtures():
     return fpl_data
 
 
-# get all fixture data for fixture
-def fixture_stats_by_fixture(fixture_id):
+# get all fixture data for a given fixture
+def fixture_stats(fixture_id):
     all_fixtures = fpl_fixtures()
     fixture = {}
     for dict in all_fixtures:
@@ -38,12 +38,24 @@ def fixture_stats_by_fixture(fixture_id):
     return fixture
 
 
-def fixture_stats_by_stat(stat_name, event):
+# get specific stats for a gameweek i.e goals scored for gameweek 1
+def gameweek_stats(stat_name, gameweek):
     all_fixtures = fpl_fixtures()
     stat_data = []
+    stats_final = []
     for dict in all_fixtures:
-        if dict["event"] == event:
+        if dict["event"] == gameweek:
             for innerdict in dict["stats"]:
                 if innerdict["identifier"] == stat_name:
-                    stat_data.append(dict)
-    return stat_data
+                    stat_data.append([innerdict, dict["code"]])
+
+    for stats in stat_data:
+        if len(stats[0]["a"]) > 0:
+            stats_final.append([stats[0]["a"], stats[1]])
+        if len(stats[0]["h"]) > 0:
+            stats_final.append([stats[0]["h"], stats[1]])
+
+    if len(stats_final) == 0:
+        return 0
+    else:
+        return stats_final
